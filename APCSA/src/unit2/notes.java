824623688478eb192fbb719fbbe8dd0df4c3e55f
jake <jakekimds@gmail.com>
2018-02-06 14:37:38 -0800

    notes update

--- APCSA/src/unit2/notes.java
+++ APCSA/src/unit2/notes.java
@@ -2,9 +2,12 @@ package unit2;
 
 public class notes {
 	static float fNum = 4.48594f;
-	static String output = "";
+	static String output = "harry";
+	static int number = 0;
 
 	public static void main(String[] args) {
-		output = String.format("%.2f", fNum);
+		System.out.print(output.charAt(0) + " ");
+		System.out.print(output.charAt(output.length() - 1) + " ");
+		System.out.println(output.substring(1, output.length() - 1));
 	}
 }
