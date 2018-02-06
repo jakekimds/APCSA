658bb7619f94074646085964705cd8b13be78b75
jake <jakekimds@gmail.com>
2018-02-06 14:06:57 -0800

    unit2 notes start

---
+++ APCSA/src/unit2/notes.java
@@ -0,0 +1,10 @@
+package unit2;
+
+public class notes {
+	static float fNum = 4.48594f;
+	static String output = "";
+
+	public static void main(String[] args) {
+		output = String.format("%.2f", fNum);
+	}
+}
