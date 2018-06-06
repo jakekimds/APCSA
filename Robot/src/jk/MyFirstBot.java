1d080474cd1bd2f1af81abf8fd2b0b30c4eae8f2
kimy3243 <kimy3243@CA-SU-F106-10.sduhsd.lan>
2018-06-06 14:58:10 -0700

    fghgfh

--- Robot/src/jk/MyFirstBot.java
+++ Robot/src/jk/MyFirstBot.java
@@ -6,6 +6,9 @@ public class MyFirstBot extends Robot {
 	/**
 	 * run: Robot's default behavior
 	 */
+	
+	double lastHead;
+	
 	public void run() {
 		// Initialization of the robot should be put here
 
@@ -17,7 +20,19 @@ public class MyFirstBot extends Robot {
 		// Robot main loop
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
-			turnGunRight(5);
+			//turnGunRight(1);
+			//fire(3);
+//			if(getEnergy() < 100){
+//				System.out.println("Jake's robot cannot die");
+//				long millis = System.currentTimeMillis();
+//				while(true){
+//					if(System.currentTimeMillis() - millis >= 2000){
+//						break;
+//					}
+//				}
+//			}
+			turnRadarRight(1000);
+			scan();
 		}
 	}
 
@@ -25,20 +40,44 @@ public class MyFirstBot extends Robot {
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-		// Replace the next line with any behavior you would like
+		double bear = e.getBearing();
+		lastHead = bear;
+		double rang = bear - getGunHeading() + getHeading();
+		rang %= 360;
+		double lang = 360 - rang;
+		lang %= 360;
+		if(lang > rang){
+			turnGunRight(rang);
+		}else{
+			turnGunLeft(lang);
+		}
+		fire(3);
+		
+		turnRight(bear + 90);
+		ahead(10000);
+		
+		
+		//hitWall = false;
+		//turnLeft(ang);
 	}
 
+	public void onDeath(DeathEvent e){
+	//	while(true){}
+	}
+	
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
 	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
+//		turnRight(e.getBearing());
+//		fire(e.getPower());
 	}
 	
 	/**
 	 * onHitWall: What to do when you hit a wall
 	 */
 	public void onHitWall(HitWallEvent e) {
-		// Replace the next line with any behavior you would like
+		turnRight(lastHead);
+		ahead(100);
 	}	
 }
